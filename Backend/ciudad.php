c05f4cb87756430b6b3a36f4111b17b66affabac
HaroldRodriguezMendoza <55288042+HaroldRodriguezMendoza@users.noreply.github.com>
2019-09-18 16:05:57 -0500

    Harold

--- Backend/ciudad.php
+++
@@ -1,5 +0,0 @@
-<?php
-  require('./libreria.php'); //Libreria requerida para el funcionamiento.
-  $getData = leerDatos(); //Lee los datos
-  obtnciudad($getData) //Ejecuta y obtiene la ciudad
- ?>
--- Backend/tipo.php
+++
@@ -1,5 +0,0 @@
-<?php
-  require('./libreria.php');
-  $getData = leerDatos(); //Leer la informacion del archivo json.
-  obtnTipo($getData)
- ?>
